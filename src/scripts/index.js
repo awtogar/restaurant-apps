76b60d4bf800a1eb6a43389a86e7fd4e28b1c5be
garebizzare <awaludintogar@gmail.com>
2024-04-17 10:27:39 +0700

    layouting
    layouting and need to add some aria to mkae more accesible

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,21 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
+import '../styles/tablet.css';
+import '../styles/desktop.css';
+import
+{
+    createIcons,
+    Menu,
+    ArrowRight,
+    Globe
+} from 'lucide';
 
 console.log('Hello Coders! :)');
+
+createIcons({
+    icons: {
+        Menu,
+        ArrowRight,
+        Globe
+    }
+});
