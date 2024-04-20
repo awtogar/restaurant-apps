5344805988ac4205dfa2aa0a76bf9c1b92cee2ad
garebizzare <awaludintogar@gmail.com>
2024-04-21 01:51:02 +0700

    INITAL DONE
    add some responsive and fixing desc too long and cant load image from url

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,21 +1,43 @@
-import 'regenerator-runtime'; /* for async await transpile */
+import 'regenerator-runtime';
 import '../styles/main.css';
 import '../styles/tablet.css';
 import '../styles/desktop.css';
-import
-{
-    createIcons,
-    Menu,
-    ArrowRight,
-    Globe
-} from 'lucide';
+import 'remixicon/fonts/remixicon.css';
+import myData from '../public/data/DATA.json';
 
-console.log('Hello Coders! :)');
+const restaurantItem = (data) => {
+    data.restaurants.forEach((restaurant) => {
+        const restaurantList = document.querySelector('.restaurant-list');
+        restaurantList.innerHTML += `
 
-createIcons({
-    icons: {
-        Menu,
-        ArrowRight,
-        Globe
+
+        <article class="restaurant-item">
+          <div class="item-thumbnail">
+            <p class="city">${restaurant.city}</p>
+            <img class="restaurant-images" src="${restaurant.pictureId}"
+              alt="footage of restaurant at ${restaurant.name}]">
+          </div>
+          <div class="item-header">
+            <h2 class="restaurant-name">${restaurant.name}</h2>
+            <div class="rating">
+              <i class="ri-star-fill"></i>
+              <p class="rating-value">${restaurant.rating}</p>
+            </div>
+          </div>
+          <p class="item-description">${restaurant.description}</p>
+        </article>
+        `;
+    });
+}
+
+restaurantItem(myData); // Memanggil fungsi dengan data yang diimpor\
+
+
+document.getElementById('hamburger').addEventListener('click', function () {
+    var drawer = document.getElementById('drawer');
+    if (drawer.style.display === 'block') {
+        drawer.style.display = 'none';
+    } else {
+        drawer.style.display = 'block';
     }
 });
