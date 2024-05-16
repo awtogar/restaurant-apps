e4f01c285d92112cc432a528110e2a7922d10987
Awaluddin Togar <107783669+awtogar@users.noreply.github.com>
2024-05-16 16:35:23 +0700

    Update config.js

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,11 +1,12 @@
-const CONFIG = {
-    BASE_URL: 'https://restaurant-api.dicoding.dev',
+const const CONFIG = {
+    BASE_URL: 'https://restaurant-api.dicoding.dev/',
     BASE_IMAGE_URL: {
         small: 'https://restaurant-api.dicoding.dev/images/small/',
         medium: 'https://restaurant-api.dicoding.dev/images/medium/',
         large: 'https://restaurant-api.dicoding.dev/images/large/'
     },
-    DEFAULT_LANGUAGE: 'id', // Gw asumsikan default bahasa Indonesia
+DATABASE_NAME: 'restaurant-database',
+DATABASE_VERSION: 1,
 };
 
 export default CONFIG;
